4146bdf1634a061dfab2b1c5a2cdd5055969d9cd
Matthew Pence <matt@ohiosveryown.co>
2020-07-11 01:03:37 -0400

    form test

--- components/About.vue
+++ components/About.vue
@@ -5,11 +5,31 @@
       <svg class="close" width="30" height="29" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M15.707 14.142l6.718-6.717-.707-.707L15 13.435 8.282 6.718l-.707.707 6.718 6.717-6.718 6.718.707.707L15 14.849l6.718 6.718.707-.707-6.718-6.718z" fill="#000"/></svg>
     </header>
 
-    <article class="f--us">
+    <!-- <article class="f--us">
       <p>menu.black was created by me, <a class="link" target="_blank" href="https://twitter.com/cmykw_">Matt</a>. I live in Atlanta, I love food and I wanted to make something to help create awareness and celebrate the diversity of Atlanta and it’s food. You can view the <a class="link" target="_blank" href="https://github.com/ohiosveryown/menu.black-2020">source here</a>.</p>
       <p>Thanks to <a class="link" target="_blank" href="https://twitter.com/imanashanti">@imanashanti</a> for helping curate the list!</p>
       <p>Food brings people together, and good food keeps people coming back. If you’d like to submit a spot, <a class="link" target="_blank" href="https://forms.gle/GbfRiMT4yZRxv9Ry9">contact here</a>. Be kind to one another, and enjoy the food.</p>
-    </article>
+    </article> -->
+<form name="contact" method="POST" data-netlify="true">
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Your Role: <select name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Follower</option>
+    </select></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
     <img src="../static/img/veggies/veggies-02.png" alt="peppers">
   </section>
 </template>
